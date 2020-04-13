a195fc936f5bb7f5fffb423d779c726138e30aff
Ali <ali_elsebaie98@yahoo.com>
2020-04-13 02:36:06 +0200

    commit

--- read.cpp
+++ read.cpp
@@ -2,36 +2,44 @@
 #include <pcl/visualization/cloud_viewer.h>
 #include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
+#include <fstream>
+#include <string>
+
+#include <dirent.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <experimental/filesystem>
+
 
 int main (int argc, char** argv){
     
-    //pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+  namespace fs = std::experimental::filesystem;
+  std::string path = "../pcd_files";
+
+  for (const auto & entry : fs::directory_iterator(path)){
+    std::cout << entry.path().filename() << std::endl;
 
     pcl::PointCloud<pcl::PointXYZI>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZI>);
 
-    // pcl::visualization::PointCloudColorHandlerGenericField<pcl::PointXYZI> intensity_distribution(cloud,"intensity");
-
-    // viewer->addPointCloud<pcl::PointXYZI> (cloud, intensity_distribution, "source");
-
-
-
-  if (pcl::io::loadPCDFile<pcl::PointXYZI> ("/home/mohamedaboushenif/Desktop/rosbags/rosbag1/1585830610.770500032.pcd", *cloud) == -1){
+    if (pcl::io::loadPCDFile<pcl::PointXYZI> 
+    (entry.path(), *cloud) == -1){
     PCL_ERROR ("Couldn't read file test_pcd.pcd \n");
     return (-1);
-  }
+    }
 
-
-
-
-
-  std::cout << "Loaded "
+    std::cout << "Loaded "
             << cloud->width * cloud->height
             << " data points from test_pcd.pcd with the following fields: "
             << std::endl;
-  FILE * stream; 
-  stream = fopen ("pcdfile.bin","wb");
-         
-  for (std::size_t i = 0; i < cloud->points.size (); ++i){
+    
+    std::stringstream ss;
+    ss << "../bin_files" << "/" << entry.path().filename() << ".bin";
+
+    FILE * stream; 
+    stream = fopen (ss.str().c_str() ,"wb");
+
+    for (std::size_t i = 0; i < cloud->points.size (); ++i){
     std::cout << "    " << cloud->points[i].x
               << " "    << cloud->points[i].y
               << " "    << cloud->points[i].z 
@@ -41,7 +49,10 @@ int main (int argc, char** argv){
     fwrite(data,sizeof(float),sizeof(data),stream);
 
   }
-  fclose(stream);
+    fclose(stream);
+
+  }
+
 
 
 
